35569922e66df3dd4ff1f49437cba2c4c8a3d4cf
christophemilliere <christophemilliere93@gmail.com>
2017-07-01 22:36:32 +0200

    create pipe days ago with date-fns

--- src/app/job-add-form/job-add-form.component.ts
+++ src/app/job-add-form/job-add-form.component.ts
@@ -14,7 +14,32 @@ export class JobAddFormComponent implements OnInit {
     { id: 3, name: 'contrat à durée déterminée (CDD)', value: 'fixed-term' },
     { id: 4, name: 'contrat à durée indéterminée (CDI)', value: 'permanent' },
     { id: 5, name: 'indépendant', value: 'freelance' }
-  ]
+  ];
+
+  currencies = [
+    {id: 1, name: 'euros', value: 'EU', symbol: '€'},
+    {id: 2, name: 'livres sterling', value: 'POUNDS', symbol: '£'},
+    {id: 3, name: 'francs CFA', value: 'CFA', symbol: 'CFA'},
+    {id: 4, name: 'dollars canadien', value: 'CAD', symbol: '$'}
+  ];
+
+  statuses = [
+    {id: 1, name: 'cadre', value: 'executive'},
+    {id: 1, name: 'employé', value: 'employee'}
+  ];
+
+  experience = [
+    { id: 1, name: 'junior', value: 'junior'},
+    { id: 2, name: 'medior', value: 'medior'},
+    { id: 3, name: 'senior', value: 'senior'}
+  ];
+
+  areas = [
+    {id: 1, name: 'aucun déplacements', value: 'none'},
+    {id: 2, name: 'déplacements régionaux', value: 'region'},
+    {id: 3, name: 'déplacements nationaux', value: 'nation'},
+    {id: 4, name: 'déplacements internationaux', value: 'international'}
+  ];
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -26,7 +51,7 @@ export class JobAddFormComponent implements OnInit {
       zipcode: 35,
       description: '',
       contract: '',
-      salary: 0,
+      salary: null,
       currency: '',
       startdate: new Date(),
       experience: '',
